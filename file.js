6e1c3e99779b82884fb966af42e54d81c0450795
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2023-07-20 22:26:24 +0200

    [script] Mouse interactivity

--- file.js
+++ file.js
@@ -7,7 +7,18 @@ canvas.height = 500;
 let score = 0; //рахунок
 let gameFrame = 0; //гра Рамка
 ctx.font = '50px Georgia';
+
 //Mouse interactivity
+let canvasPosition = canvas.getBoundingClientRect();
+const mouse = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    click: false
+}
+canvas.addEventListener('mousedown', function(event){
+    mouse.x = event.x - canvasPosition.left;
+    mouse.y = event.y - canvasPosition.top;
+});
 //Player
 //Bubbles
 //Animation loop
