07c563f605f056429ecded2a1b8a63427bd149bb
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2023-07-20 22:03:36 +0200

    [script]Canvas setup

--- file.js
+++ file.js
@@ -1,5 +1,14 @@
 //Canvas setup
+const canvas = document.querySelector('#canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
+
+let score = 0; //рахунок
+let gameFrame = 0; //гра Рамка
+ctx.font = '50px Georgia';
 //Mouse interactivity
 //Player
 //Bubbles
 //Animation loop
+
